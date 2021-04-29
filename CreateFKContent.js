4b1eb2f5b6e946aa90b4c7baefda07bde31580db
Jai Ranganathan <jai.ranganathan@gmail.com>
2021-04-29 01:06:31 -0400

    Update CreateFKContent.js

--- CreateFKContent.js
+++ CreateFKContent.js
@@ -1,20 +1,20 @@
-// This function generates the modal window content for the Florida Keys ESR site
+// This is a helper function used by createFKContent (below) to find the column number in a google spreadsheet of a given column name 
+function getColumnIndex(dataTable, columnName){
+  var i = -1;
+    do {
+      i = i +1;
+    } while (columnName != dataTable.getColumnLabel(i));
+  return i;
+}
 
+// This function generates the modal window content for the Florida Keys ESR site
 function createFKContent(DataSourceURL, icon){
   var query = new google.visualization.Query(DataSourceURL);
   query.send(handleQueryResponse);
 
   function handleQueryResponse(response) {
-  
-    var GoogleData = response.getDataTable();
 
-    function getColumnIndex(dataTable, columnName){
-      var i = -1;
-        do {
-          i = i +1;
-        } while (columnName != dataTable.getColumnLabel(i));
-      return i;
-    }
+    var GoogleData = response.getDataTable();
     var rowNumber = GoogleData.getFilteredRows([{column: getColumnIndex(GoogleData, "icon"), value: icon}])[0];
     var caption = GoogleData.getValue(rowNumber, getColumnIndex(GoogleData, "caption"));
     var provider_link = GoogleData.getValue(rowNumber, getColumnIndex(GoogleData, "provider_link"));
@@ -23,25 +23,28 @@ function createFKContent(DataSourceURL, icon){
     var YAxisTitle = GoogleData.getValue(rowNumber, getColumnIndex(GoogleData, "y_label"));
     var YAxis2Title = GoogleData.getValue(rowNumber, getColumnIndex(GoogleData, "y2_label"));
     var SpreadsheetLink = GoogleData.getValue(rowNumber, getColumnIndex(GoogleData, "data"));
-    
-    console.log(YAxis2Title == null);
 
-    document.getElementById('caption').innerHTML = caption;
-    document.getElementById('title').innerHTML = title;
+    if (SpreadsheetLink == null){
+      document.getElementById('chart_div').innerHTML = "<em>No data</em> yet assigned to icon <b>" + icon + "</b>. This needs to be specified in the Google Sheet 'icons to data | fk-esr-info'.";
+    }
+    else {
+      document.getElementById('caption').innerHTML = caption;
+      document.getElementById('title').innerHTML = title;
 
-    var node = document.createElement("a");                
-    var textnode = document.createTextNode(source);    
-    node.appendChild(textnode);             
-    node.href = provider_link;  
-    node.target = "_parent";         
-    document.getElementById("source").appendChild(node);
+      var node = document.createElement("a");                
+      var textnode = document.createTextNode(source);    
+      node.appendChild(textnode);             
+      node.href = provider_link;  
+      node.target = "_parent";         
+      document.getElementById("source").appendChild(node);
 
-    GraphWrapper({Google_Spreadsheet_Link: SpreadsheetLink,
-      YAxisTitle: YAxisTitle,
-      threshold_year: 2011,
-      SST: (icon == "SST"),
-      twoYAxes: (YAxis2Title != null),
-      YAxisTitle2: YAxis2Title
-    });
+      GraphWrapper({Google_Spreadsheet_Link: SpreadsheetLink,
+        YAxisTitle: YAxisTitle,
+        threshold_year: 2011,
+        SST: (icon == "SST"),
+        twoYAxes: (YAxis2Title != null),
+        YAxisTitle2: YAxis2Title
+      });
+    }
   }
 }
